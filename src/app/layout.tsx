892075e41a5952c97f277140e46b263fc8fbd6e0
Antoine <antoine.devillers01@gmail.com>
2025-09-04 12:01:23 +0200

    fix favicaon affichage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#10b981" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link 
